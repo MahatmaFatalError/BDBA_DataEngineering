e62a6821e36359eb31bcb4a2fa41d1b26756c433
Johannes Weber <johannesweber@users.noreply.github.com>
2018-01-13 20:37:30 +0100

    Initial commit

---
+++ sodata.py
@@ -0,0 +1,6 @@
+from kafka import KafkaProducer
+from sodapy import Socrata
+
+client = Socrata("data.cityofnewyork.us", "vWSR699ElPnWWrJDH90VsNXij", username="weber.johanes@gmail.com", password="test#123")
+metadata = client.get("fhrw-4uyv", content_type="json", limit=2)
+print(metadata)
