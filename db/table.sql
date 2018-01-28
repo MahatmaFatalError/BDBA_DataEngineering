c735c866053f7b23c27474bde46c5bc9a63a25e1
Ruppel <Julian.Ruppel@snp-ag.com>
2018-01-28 20:04:10 +0100

    PK added

--- db/table.sql
+++ db/table.sql
@@ -4,13 +4,13 @@
 
 CREATE TABLE public.service_request
 (
-    created_date timestamp with time zone,
+    id bigserial NOT NULL,    
+	created_date timestamp with time zone,
     agency_name character(255) COLLATE pg_catalog."default",
     complaint_type character(255) COLLATE pg_catalog."default",
     descriptor character(255) COLLATE pg_catalog."default",
     longitude double precision,
     latitude double precision,
-    id bigint NOT NULL DEFAULT nextval('service_request_id_seq'::regclass),
     agency character(255) COLLATE pg_catalog."default",
     location_type character(255) COLLATE pg_catalog."default",
     incident_zip character(10) COLLATE pg_catalog."default",
@@ -23,7 +23,8 @@ CREATE TABLE public.service_request
     status character(50) COLLATE pg_catalog."default",
     due_date timestamp with time zone,
     borough character(100) COLLATE pg_catalog."default",
-    resolution_description text COLLATE pg_catalog."default"
+    resolution_description text COLLATE pg_catalog."default",
+    CONSTRAINT pk_id PRIMARY KEY (id)
 )
 WITH (
     OIDS = FALSE
