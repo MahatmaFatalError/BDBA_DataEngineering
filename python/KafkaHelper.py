a0fc7484bf75d8fd76698b51bbcd0739f9d48da9
Johannes Weber <johannesweber@users.noreply.github.com>
2018-02-03 20:12:36 +0100

    added Data Ingestion - API

--- python/KafkaHelper.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaProducer
-
-
-class BDBAProducer:
-        def __init__(self):
-            self.producer = KafkaProducer()
-
-        def send_service_request(self, topic, content):
-            self.producer.send(topic, content)
