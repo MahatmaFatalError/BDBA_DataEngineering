2d92f65c1c6454b8c1f78b30c624f31cdfd644e3
Mahatma_Fatal_Error <MahatmaFatalError@users.noreply.github.com>
2018-02-05 21:07:09 +0100

    topic change

--- java/src/main/java/com/srh/bdba/dataengineering/KafkaCommons.java
+++ java/src/main/java/com/srh/bdba/dataengineering/KafkaCommons.java
@@ -6,6 +6,6 @@ package com.srh.bdba.dataengineering;
  *
  */
 public interface KafkaCommons {
-	final static String TOPIC = "test";
+	final static String TOPIC = "ServiceRequests";
 	final static String BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094";
 }
