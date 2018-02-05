794d0e6ce20d1b7e97bfbdcddba966f707d3d471
Mahatma_Fatal_Error <MahatmaFatalError@users.noreply.github.com>
2018-02-05 20:48:37 +0100

    consider main args params

--- java/src/main/java/com/srh/bdba/dataengineering/Main.java
+++ java/src/main/java/com/srh/bdba/dataengineering/Main.java
@@ -1,5 +1,7 @@
 package com.srh.bdba.dataengineering;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * starts both a produces and a consumer thread in parallel
  * @author jruppel
@@ -7,9 +9,20 @@ package com.srh.bdba.dataengineering;
  */
 public class Main {
 
+	/**
+	 * 
+	 * @param args path to csv file
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 
-		MyProducer producer = new MyProducer("/data/311_Service_Requests_from_2010_to_Present.csv");
+		String csvFilePath = "/data/311_Service_Requests_from_2010_to_Present.csv";
+
+		if (args != null && args.length > 0 && !StringUtils.isBlank(args[0])){
+			csvFilePath = args[0];
+		}
+
+		MyProducer producer = new MyProducer(csvFilePath);
 		MyConsumer consumer = new MyConsumer();
 
 		new Thread(producer).start();
